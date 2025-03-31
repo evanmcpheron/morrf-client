b8d8a674f605c55981d65a3d81261506912bbbbc
Evan McPheron <evanmcpheron@MT20XG2LY4.local>
2025-03-30 17:29:19 -0700

    init versioning

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,15 +1,15 @@
-import { Link } from "react-router";
-import { usePolicy } from "../PolicyProvider";
+import { Link } from 'react-router';
+import { usePolicy } from '../PolicyProvider';
 
 export const Navigation = () => {
   const { role, hasAnyPolicy } = usePolicy();
 
   const displayLogout = () => {
-    if (role !== "GUEST") {
+    if (role !== 'GUEST') {
       return (
         <Link
           onClick={() => {
-            localStorage.setItem("role", "GUEST");
+            localStorage.setItem('role', 'GUEST');
           }}
           to="/login"
         >
@@ -20,30 +20,18 @@ export const Navigation = () => {
   };
 
   return (
-    <div style={{ display: "flex", flexDirection: "row", gap: "1rem" }}>
+    <div style={{ display: 'flex', flexDirection: 'row', gap: '1rem' }}>
       <div>{role}</div>
       <Link to="/">Dashboard</Link>
-      {hasAnyPolicy(["VIEW_ORDERS", "EDIT_ORDERS"]) && (
-        <Link to="/orders">Orders</Link>
-      )}
-      {hasAnyPolicy(["VIEW_CUSTOMERS", "EDIT_CUSTOMERS"]) && (
-        <Link to="/customers">Customers</Link>
-      )}
-      {hasAnyPolicy(["VIEW_REPORTS", "EDIT_REPORTS"]) && (
-        <Link to="/reports">Reports</Link>
-      )}
-      {hasAnyPolicy(["VIEW_SETTINGS", "EDIT_SETTINGS"]) && (
-        <Link to="/settings">Settings</Link>
-      )}
-      {hasAnyPolicy(["VIEW_USERS", "EDIT_USERS"]) && (
-        <Link to="/users">Users</Link>
-      )}
-      {hasAnyPolicy(["VIEW_PRODUCTS", "EDIT_PRODUCTS"]) && (
-        <Link to="/products">Products</Link>
-      )}
+      {hasAnyPolicy(['VIEW_ORDERS', 'EDIT_ORDERS']) && <Link to="/orders">Orderss</Link>}
+      {hasAnyPolicy(['VIEW_CUSTOMERS', 'EDIT_CUSTOMERS']) && <Link to="/customers">Customers</Link>}
+      {hasAnyPolicy(['VIEW_REPORTS', 'EDIT_REPORTS']) && <Link to="/reports">Reports</Link>}
+      {hasAnyPolicy(['VIEW_SETTINGS', 'EDIT_SETTINGS']) && <Link to="/settings">Settings</Link>}
+      {hasAnyPolicy(['VIEW_USERS', 'EDIT_USERS']) && <Link to="/users">Users</Link>}
+      {hasAnyPolicy(['VIEW_PRODUCTS', 'EDIT_PRODUCTS']) && <Link to="/products">Products</Link>}
       <Link to="/login">Login</Link>
 
-      <nav style={{ display: "flex", gap: "1rem" }}>{displayLogout()}</nav>
+      <nav style={{ display: 'flex', gap: '1rem' }}>{displayLogout()}</nav>
     </div>
   );
 };
